ca193ac5b9bc66e9965c22741d33bc720022ee87
Osanda Muthukumarana <it22170798@my.sliit.lk>
2026-01-25 11:53:44 +0530

    commit -- temp

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const adminRoutes = require('./routes/adminRoute');
+const authRoutes = require('./routes/authRoute.js');
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/admin', adminRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
